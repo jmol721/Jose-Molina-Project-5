d05bc4d8439df6362bc65324e86de2a1316eaa30
jose <jmolina721@gmail.com>
2021-04-25 10:14:57 -0500

    Color change property: Added the logic to change the color of the textarea depending on the time. Also, cleaned up HTML code for styling purposes.

--- Develop/style.js
+++ Develop/style.js
@@ -1,45 +1,63 @@
 var displayDate = document.getElementById('currentDay');
+var times = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
 //Display the date under header
 displayDate.textContent = moment().format('dddd, MMMM Do');
 
 //Add text to box
 var createEvent = function() {
-    $("form-control").val();
+    $("textarea").val();
     console.log(createEvent);
 };
 
 //button functionality and storing values to local storage
 $(".saveBtn").on("click", function(event) {
-    console.log(event.target.id);
-    console.log($(this).siblings(".form-control")[0].value);
+    console.log('ID: ', event.target.id);
+    console.log('Input value: ', $(this).siblings("textarea")[0].value);
 
-    var hour = $(this).siblings(".hour")[0].textContent;  // 9AM
-    var description = $(this).siblings(".form-control")[0].value;  // whatevere you text
-    localStorage.setItem(hour, description);
+    // var hour = $(this).siblings(".hour")[0].textContent;  // 9
+    var hourKey = event.target.id; // span id
+    var description = $(this).siblings("textarea")[0].value;  // whatever you text
+    localStorage.setItem(hourKey, description);
 });
 
 //load tasks on refresh
 var loadEvents = function() {
-    // $(this).siblings(".hour")[0].textContent = localStorage.getItem()
-    // 9am
-    var times = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM" ];
+    // var times = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM" ];
     for (var i = 0; i < times.length; i++) {
+        var inputId = times[i] + "-input";   // {time}-input
         if (localStorage.getItem(times[i]) !== null) {
         console.log(localStorage.getItem(times[i]));
-        document.getElementById(times[i]).value = localStorage.getItem(times[i]);
+        document.getElementById(inputId).value = localStorage.getItem(times[i]);
         };
+
+        //color change
+        var currentHour = moment().hour();
+        console.log(currentHour);
+        if (currentHour === parseInt(times[i])) {
+            document.getElementById(inputId).classList.remove("past");
+            document.getElementById(inputId).classList.remove("future");
+        }
+        else if (currentHour > parseInt(times[i])) {
+            document.getElementById(inputId).classList.remove("present");
+            document.getElementById(inputId).classList.remove("future");
+        } 
+        else {
+        document.getElementById(inputId).classList.remove("past");
+        document.getElementById(inputId).classList.remove("present");
+        }
     }
 
+    console.log(currentHour);
+    return console.log(currentHour);
 }
 
 loadEvents();
 
-// var eventEl = function() {
-// document.createElement('span');
-// eventEl.setAttribute("class", "description");
-// eventEl.textContent = localStorage.getItem(hour, description);
-// console.log(eventEl);
 
-// document.getElementById("input-group").appendChild(eventEl);
-// }
+//color change function
+// var currentHour = moment().hour();
+// console.log(currentHour);
+
+
+// document.getElementById(times[i]).setClass(".present")
